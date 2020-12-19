748a8928e6711d10fd32268346639528c2ed30cf
Neil Walker <neil@wynded.co.uk>
2020-12-19 12:31:16 +0000

    First version in git
    New project created and VS Code configured

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace playlist_editor.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace playlist_editor.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
