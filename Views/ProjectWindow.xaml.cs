03641ac68a1613f0bf0d97ddeb2580b26d7add89
Neil Walker <neil@wynded.co.uk>
2020-12-19 15:49:40 +0000

    Add a drop area to receive files

--- Views/ProjectWindow.xaml.cs
+++ Views/ProjectWindow.xaml.cs
@@ -1,5 +1,7 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using PlaylistEditor.Models;
 
@@ -13,9 +15,35 @@ namespace PlaylistEditor.Views
             set { _project = value; Title = value.ProjectDirectory; }
         }
 
+        TextBlock _FileDropArea;
+
         public ProjectWindow()
         {
             InitializeComponent();
+
+            _FileDropArea = this.Find<TextBlock>("FileDropArea");
+
+            // Set-up drag and drop
+            AddHandler(DragDrop.DragOverEvent, DragOver);
+            AddHandler(DragDrop.DropEvent, Drop);
+        }
+
+        void DragOver(object sender, DragEventArgs e)
+        {
+            // Only allow Copy or Link as Drop Operations.
+            e.DragEffects = e.DragEffects & (DragDropEffects.Copy | DragDropEffects.Link);
+
+            // Only allow if the dragged data contains filenames.
+            if (!e.Data.Contains(DataFormats.FileNames)) {
+                e.DragEffects = DragDropEffects.None;
+            }
+        }
+
+        void Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.FileNames)) {
+                _FileDropArea.Text = string.Join(Environment.NewLine, e.Data.GetFileNames());
+            }
         }
 
         private void InitializeComponent()
