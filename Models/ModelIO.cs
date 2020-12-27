8b1ce165219e3be4df32d01df8a9ff87ee6f0cd9
Neil Walker <neil@wynded.co.uk>
2020-12-27 14:11:12 +0000

    Partially implement POCO-based saving
    Doesn't yet save connections

--- Models/ModelIO.cs
+++ Models/ModelIO.cs
@@ -1,17 +1,62 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace PlaylistEditor.Models
 {
+    public class MusicFilePOCO
+    {
+        public string SourceFile;
+        public double CanvasX;
+        public double CanvasY;
+        public MusicFilePOCO()
+        {
+            SourceFile = "";
+            CanvasX = CanvasY = 0;
+        }
+        public MusicFilePOCO(string sf, double x, double y)
+        {
+            SourceFile = sf;
+            CanvasX = x;
+            CanvasY = y;
+        }
+    }
+
+    public class ProjectPOCO
+    {
+        public string ProjectDirectory;
+        public List<MusicFilePOCO> MusicFiles;
+        public ProjectPOCO()
+        {
+            ProjectDirectory = "";
+            MusicFiles = new List<MusicFilePOCO>();
+        }
+
+        public ProjectPOCO(string dir, List<MusicFilePOCO> files)
+        {
+            ProjectDirectory = dir;
+            MusicFiles = files;
+        }
+    }
+
+
     public class ModelIO {
         public static void SaveProject(Project project)
         {
+            var musicFilePocos = new List<MusicFilePOCO>();
+            foreach (var mf in project.MusicFiles)
+            {
+                musicFilePocos.Add(new MusicFilePOCO(mf.SourceFile, mf.CanvasX, mf.CanvasY));
+            }
+            var projectPOCO = new ProjectPOCO(project.ProjectDirectory, musicFilePocos);
+
             var options = new JsonSerializerOptions {
+                IncludeFields = true,
                 WriteIndented = true,
-                ReferenceHandler = ReferenceHandler.Preserve,
             };
-            string jsonString = JsonSerializer.Serialize(project, options);
+
+            string jsonString = JsonSerializer.Serialize(projectPOCO, options);
             File.WriteAllText(project.contentsFile, jsonString);
         }
 
@@ -21,9 +66,25 @@ namespace PlaylistEditor.Models
             string jsonString = File.ReadAllText(filename);
             var options = new JsonSerializerOptions {
                 IncludeFields = true,
-                ReferenceHandler = ReferenceHandler.Preserve,
             };
-            Project project = JsonSerializer.Deserialize<Project>(jsonString, options);
+            ProjectPOCO projectPOCO;
+            try {
+                projectPOCO = JsonSerializer.Deserialize<ProjectPOCO>(jsonString, options);
+            }
+            catch (System.InvalidOperationException e)
+            {
+                System.Diagnostics.Trace.WriteLine($"{e}");
+                throw;
+            }
+            Project project = new Project();
+            project.ProjectDirectory = projectPOCO.ProjectDirectory;
+            foreach (var mfPOCO in projectPOCO.MusicFiles)
+            {
+                var mf = new MusicFile(project, mfPOCO.SourceFile);
+                mf.CanvasX = mfPOCO.CanvasX;
+                mf.CanvasY = mfPOCO.CanvasY;
+                project.AddMusicFile(mf);
+            }
             return project;
         }
     }
