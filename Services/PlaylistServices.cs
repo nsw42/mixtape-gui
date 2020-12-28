aa6fdcaf72ba3b59d875c2cc52f1ebb0722dfb6d
Neil Walker <neil@wynded.co.uk>
2020-12-28 10:55:50 +0000

    Implement simple m3u import

---
+++ Services/PlaylistServices.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using PlaylistEditor.Models;
+
+namespace PlaylistEditor.Services
+{
+    public class PlaylistService
+    {
+        public static void Import(Project project, string m3ufilename)
+        {
+            string playlistContent = File.ReadAllText(m3ufilename);
+            string[] lines = playlistContent.Split('\r', System.StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                if (line.StartsWith('#'))
+                {
+                    continue;
+                }
+                project.AddMusicFile(new MusicFile(project, line));
+            }
+        }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using PlaylistEditor.Models;
+using PlaylistEditor.Services;
 using PlaylistEditor.Views;
 
 namespace PlaylistEditor.ViewModels
@@ -23,9 +24,11 @@ namespace PlaylistEditor.ViewModels
                 {
                     var result = await dialog.ShowAsync(desktop.MainWindow);
                     if (result != null) {
-                        OpenProject(result, create: true);
+                        return OpenProject(result, create: true);
                     }
                 }
+
+                return null;
             });
 
             OpenExistingProjectCommand = ReactiveCommand.CreateFromTask(async () => {
@@ -36,13 +39,41 @@ namespace PlaylistEditor.ViewModels
                 {
                     var result = await dialog.ShowAsync(desktop.MainWindow);
                     if (result != null) {
-                        OpenProject(result, create: false);
+                        return OpenProject(result, create: false);
+                    }
+                }
+
+                return null;
+            });
+
+            ImportM3UPlaylistCommand = ReactiveCommand.CreateFromTask(async () => {
+                var m3uDialog = new OpenFileDialog {
+                    Title = "Select m3u file",
+                    AllowMultiple = false,
+                    Filters = new List<FileDialogFilter> {
+                        new FileDialogFilter {
+                            Name = "m3u files (.m3u)",
+                            Extensions = new List<string> {"m3u"}
+                        },
+                        new FileDialogFilter {
+                            Name = "All files",
+                            Extensions = new List<string> {"*"}
+                        }
+                    }
+                };
+
+                if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                {
+                    var m3ufiles = await m3uDialog.ShowAsync(desktop.MainWindow);
+                    if (m3ufiles != null) {
+                        // and now import
+                        CreateNewProjectCommand.Execute().Subscribe(project => PlaylistService.Import(project, m3ufiles[0]));
                     }
                 }
             });
         }
 
-        private void OpenProject(string projectDir, bool create)
+        private Project OpenProject(string projectDir, bool create)
         {
             Project project;
             if (create) {
@@ -55,9 +86,12 @@ namespace PlaylistEditor.ViewModels
                 DataContext = new ProjectViewModel(project),
             };
             window.Show();
+
+            return project;
         }
 
-        public ReactiveCommand<Unit, Unit> CreateNewProjectCommand { get; }
-        public ReactiveCommand<Unit, Unit> OpenExistingProjectCommand { get; }
+        public ReactiveCommand<Unit, Project> CreateNewProjectCommand { get; }
+        public ReactiveCommand<Unit, Project> OpenExistingProjectCommand { get; }
+        public ReactiveCommand<Unit, Unit> ImportM3UPlaylistCommand {get; }
     }
 }
